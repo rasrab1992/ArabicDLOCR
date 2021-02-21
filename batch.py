142cee1d6131e5d4fbfad1f2319df7baceb8ca50
rasool <rasrab1992@gmail.com>
2021-02-22 00:33:29 +0330

    add new batch file

---
+++ batch.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Batch:
+    "batch containing images and ground truth texts"
+
+    def __init__(self, gtTexts, imgs):
+        self.imgs = np.stack(imgs, axis=0)
+        self.gtTexts = gtTexts
